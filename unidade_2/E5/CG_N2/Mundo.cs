c501ed80b7632708e630ad01f91d66582f57e337
HenriqueJW <54775255+HenriqueJW@users.noreply.github.com>
2020-10-24 21:52:00 -0300

    Add files via upload

---
+++ unidade_2/E5/CG_N2/Mundo.cs
@@ -0,0 +1,208 @@
+﻿/**
+  Autor: Dalton Solano dos Reis
+**/
+
+#define CG_Gizmo
+// #define CG_Privado
+
+using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Collections.Generic;
+using OpenTK.Input;
+using CG_Biblioteca;
+using CG_N2;
+
+namespace gcgcg
+{
+    class Mundo : GameWindow
+    {
+        private static Mundo instanciaMundo = null;
+
+        private Mundo(int width, int height) : base(width, height) { }
+
+        public static Mundo GetInstance(int width, int height)
+        {
+            if (instanciaMundo == null)
+                instanciaMundo = new Mundo(width, height);
+            return instanciaMundo;
+        }
+
+        private CameraOrtho camera = new CameraOrtho();
+        private Camera cameraTeclado;
+
+        private SegReta palito1;
+        protected List<Objeto> objetosLista = new List<Objeto>();
+        private ObjetoGeometria objetoSelecionado = null;
+        private bool bBoxDesenhar = false;
+        int mouseX, mouseY;   //TODO: achar método MouseDown para não ter variável Global
+        private bool mouseMoverPto = false;
+        private Retangulo obj_Retangulo;
+#if CG_Privado
+    private Privado_SegReta obj_SegReta;
+    private Privado_Circulo obj_Circulo;
+#endif
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            cameraTeclado = new Camera(camera);
+            camera.xmin = -400; camera.xmax = 400; camera.ymin = -400; camera.ymax = 400;
+
+            Console.WriteLine(" --- Ajuda / Teclas: ");
+            Console.WriteLine(" [  H     ] mostra teclas usadas. ");
+
+            //Cria-se o senhor palito com as posições dos pontos fixas
+            palito1 = new SegReta("P1", null, new Ponto4D(0,0,0,1), Matematica.GerarPtosCirculo(45, 100));
+
+            //Cor preta
+            palito1.ObjetoCor.CorR = 0; palito1.ObjetoCor.CorG = 0; palito1.ObjetoCor.CorB = 0;
+
+            //Adiciona-o na lista de objetos
+            objetosLista.Add(palito1);
+            
+#if CG_Privado
+      obj_SegReta = new Privado_SegReta("B", null, new Ponto4D(50, 150), new Ponto4D(150, 250));
+      obj_SegReta.ObjetoCor.CorR = 255; obj_SegReta.ObjetoCor.CorG = 0; obj_SegReta.ObjetoCor.CorB = 0;
+      objetosLista.Add(obj_SegReta);
+      objetoSelecionado = obj_SegReta;
+
+      obj_Circulo = new Privado_Circulo("C", null, new Ponto4D(100, 300), 50);
+      obj_Circulo.ObjetoCor.CorR = 0; obj_Circulo.ObjetoCor.CorG = 255; obj_Circulo.ObjetoCor.CorB = 255;
+      objetosLista.Add(obj_Circulo);
+      objetoSelecionado = obj_Circulo;
+#endif
+            GL.ClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        }
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(camera.xmin, camera.xmax, camera.ymin, camera.ymax, camera.zmin, camera.zmax);
+        }
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+#if CG_Gizmo
+            Sru3D();
+#endif
+
+
+
+            for (var i = 0; i < objetosLista.Count; i++)
+                objetosLista[i].Desenhar();
+            if (bBoxDesenhar && (objetoSelecionado != null))
+                objetoSelecionado.BBox.Desenhar();
+            this.SwapBuffers();
+        }
+
+        protected override void OnKeyDown(OpenTK.Input.KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.H)
+                Utilitario.AjudaTeclado();
+            else if (e.Key == Key.Escape)
+                Exit();
+            else if (e.Key == Key.Space)
+            {
+                Primitivas.mudarPrimitiva();
+            }
+            else if (e.Key == Key.E)
+            {
+                // Console.WriteLine("--- Objetos / Pontos: ");
+                // for (var i = 0; i < objetosLista.Count; i++)
+                // {
+                //     Console.WriteLine(objetosLista[i]);
+                // }
+                cameraTeclado.PanEsquerda();
+            }
+            else if (e.Key == Key.D)
+            {
+
+                cameraTeclado.PanDireita();
+            }
+            else if (e.Key == Key.Q)
+            {
+                palito1.esquerda();
+            }
+            else if (e.Key == Key.W)
+            {
+                palito1.direita();
+            }
+            else if (e.Key == Key.A)
+            {
+                palito1.diminuir();
+            }
+            else if (e.Key == Key.S)
+            {
+                palito1.aumentar();
+            }
+            else if (e.Key == Key.Z)
+            {
+                palito1.girarAntiHorario();
+            }
+            else if (e.Key == Key.X)
+            {
+                palito1.girarHorario();
+            }
+            else if (e.Key == Key.C)
+            {
+                cameraTeclado.PanCima();
+            }
+            else if (e.Key == Key.B)
+            {
+                cameraTeclado.PanBaixo();
+            }
+            else if (e.Key == Key.O)
+                // bBoxDesenhar = !bBoxDesenhar;
+                cameraTeclado.ZoomOut();
+            else if (e.Key == Key.I)
+                // bBoxDesenhar = !bBoxDesenhar;
+                cameraTeclado.ZoomIn();
+            else if (e.Key == Key.V)
+                mouseMoverPto = !mouseMoverPto;   //TODO: falta atualizar a BBox do objeto
+            else
+                Console.WriteLine(" __ Tecla não implementada.");
+        }
+
+        //TODO: não está considerando o NDC
+        protected override void OnMouseMove(MouseMoveEventArgs e)
+        {
+            mouseX = e.Position.X; mouseY = 600 - e.Position.Y; // Inverti eixo Y
+            if (mouseMoverPto && (objetoSelecionado != null))
+            {
+                objetoSelecionado.PontosUltimo().X = mouseX;
+                objetoSelecionado.PontosUltimo().Y = mouseY;
+            }
+        }
+
+#if CG_Gizmo
+        private void Sru3D()
+        {
+            GL.LineWidth(2);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Convert.ToByte(255), Convert.ToByte(0), Convert.ToByte(0));
+            GL.Vertex3(0, 0, 0); GL.Vertex3(200, 0, 0);
+            GL.Color3(Convert.ToByte(0), Convert.ToByte(255), Convert.ToByte(0));
+            GL.Vertex3(0, 0, 0); GL.Vertex3(0, 200, 0);
+            GL.End();
+        }
+#endif
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mundo window = Mundo.GetInstance(600, 600);
+            window.Title = "CG_N2";
+            window.Run(1.0 / 60.0);
+        }
+    }
+}
